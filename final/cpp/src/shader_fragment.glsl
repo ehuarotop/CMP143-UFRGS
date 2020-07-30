e96eda30e42f8d3de661fff4664377d2c1ff8c42
Eduardo Gastal <eslgastal@inf.ufrgs.br>
2020-07-30 19:07:22 -0300

    Density visualization with transition based on distance.

--- final/cpp/src/shader_fragment.glsl
+++ final/cpp/src/shader_fragment.glsl
@@ -3,12 +3,19 @@
 out vec4 outColor;
 
 in vec2 TexCoord;
+in float distance_from_camera;
 
 uniform sampler2D texture1;
 
-//texture(texture1, TexCoord);
-//vec4(1.0f,0.5f, 0.9f, 1.0f);
+float gaussian(float x, float sigma) {
+    return exp(-x*x / 2 / sigma);
+}
 
 void main() {
-    outColor = texture(texture1, TexCoord);
+    float distance_from_center = length(TexCoord.xy - vec2(0.5,0.5));
+    float v = gaussian(distance_from_center, 0.05);
+    vec4 density_color = vec4(v,v,v,1); // quanto habilitar o blending: vec4(1,1,1,v);
+    vec4 image_color = texture(texture1, TexCoord);
+    float alpha = clamp(2 * gaussian(distance_from_camera, 10.0), 0, 1); // TODO: talvez encontrar valor melhor
+    outColor = mix(density_color, image_color, alpha);
 }
--- final/cpp/src/shader_vertex.glsl
+++ final/cpp/src/shader_vertex.glsl
@@ -4,6 +4,7 @@ layout (location=0) in vec3 position;
 layout (location = 1) in vec2 aTexCoord;
 
 out vec2 TexCoord;
+out float distance_from_camera;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -21,6 +22,9 @@ void main() {
 										+ cameraUpWorldSpace * position.y * billboardSize.y;
 
 	gl_Position = projection * view * vec4(vertexPosition_worldSpace, 1.0f);
+
+    vec4 camera_position_in_world_space = inverse(view) * vec4(0,0,0,1);
+    distance_from_camera = length(vec4(vertexPosition_worldSpace,1) - camera_position_in_world_space);
 
     //gl_Position = projection * view * model * vec4(position, 1.0f);
     TexCoord = vec2(aTexCoord.x, aTexCoord.y);
